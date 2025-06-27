e28fed3debb750f357fd5d22028ffd1d7740e528
Kraahk <kraahk@gmail.com>
2025-06-27 21:57:45 +0200

    fix: normalize line endings with .gitattributes

--- backend/src/routes/users.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { createUser } from '../controllers/users.controller';
-
-const router = Router();
-
-// POST /api/users
-router.post('/', createUser);
-
-export default router;
